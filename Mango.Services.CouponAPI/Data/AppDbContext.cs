424f7385946889a543949426ac0f9d48a3497b33
Samiul Haque Atul <samhaque999@gmail.com>
2025-04-10 20:10:45 +0600

    change20 created folders, configured swagger, Sql. Ready to seed DB

---
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)  //t passes those options to the base DbContext class using base(options).
+        {
+            //might include things like the connection string and DB provider (e.g., SQL Server, PostgreSQL, etc.).
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }  // creates table of Coupon model
+
+    }
+}
