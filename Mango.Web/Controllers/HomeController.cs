f1dc164b66b9de2fb6963c43e5c90dffd7625760
Samiul Haque Atul <samhaque999@gmail.com>
2025-04-18 18:52:37 +0600

    1.  .AddRazorRuntimeCompilation(); . now both the projects are running independently. 2. added custom css files to _layout.cshtml and .cshtml.css files.
    NEXT: we are going to focus on consuming the CouponAPI

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -15,7 +15,9 @@ namespace Mango.Web.Controllers
 
         public IActionResult Index()
         {
-            return View();
+
+             return View();
+        
         }
 
         public IActionResult Privacy()
---
+++ Mango.Web/Service/IService/ICouponService.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDto?> GetCouponAsync(string couponCode);
+        Task<ResponseDto?> GetAllCouponAsync();
+        Task<ResponseDto?> GetACouponByIdAsync(int id);
+        Task<ResponseDto?> CreateCouponsAsync(CouponDTO coupon);
+        Task<ResponseDto?> UpdateCouponsAsync(CouponDTO coupon);
+        Task<ResponseDto?> DeleteCouponsAsync(int id);
+    }
+}
