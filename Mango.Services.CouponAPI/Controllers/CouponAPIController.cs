a98885e6a033354a76b0dd13b4ab6153762dcf3d
Samiul Haque Atul <samhaque999@gmail.com>
2025-04-20 02:24:57 +0600

    39 coupon delete

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -151,6 +151,7 @@ namespace Mango.Services.CouponAPI.Controllers
 
 
         [HttpDelete]
+        [Route("{id:int}")]
         public ResponseDto Delete(int id)
         {
             try
--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -2,6 +2,8 @@
 using Mango.Web.Service.IService;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Mango.Web.Controllers
 {
@@ -44,6 +46,30 @@ namespace Mango.Web.Controllers
                 }
             }
             return View(model);
+        }
+
+        public async Task<IActionResult> CouponDelete(int couponId)
+        {
+			ResponseDto? response = await _couponService.GetACouponByIdAsync(couponId);
+
+			if (response != null && response.IsSuccess == true)
+			{
+				CouponDTO? model = JsonConvert.DeserializeObject<CouponDTO>(Convert.ToString(response.Result));
+                return View(model);
+			}
+			return NotFound();
+        }
+
+        [HttpPost]   //in api it is httpdelete
+        public async Task<IActionResult> CouponDelete(CouponDTO couponDto)
+        {
+            ResponseDto? response = await _couponService.DeleteCouponsAsync(couponDto.CouponId);
+
+            if (response != null && response.IsSuccess == true)
+            {
+                return RedirectToAction(nameof(CouponIndex));
+            }
+            return View(couponDto);
         }
     }
 }
