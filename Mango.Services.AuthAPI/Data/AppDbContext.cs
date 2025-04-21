7982ff12c61df08d5da88120c1c0310711c24bcb
Samiul Haque Atul <samhaque999@gmail.com>
2025-04-22 01:14:00 +0600

    43 ApplicationUser : IdentityUser Name column added to AspNetUsers

--- Mango.Services.AuthAPI/Data/AppDbContext.cs
+++ Mango.Services.AuthAPI/Data/AppDbContext.cs
@@ -1,16 +1,19 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Mango.Services.AuthAPI.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 
 namespace Mango.Services.AuthAPI.Data
 {
-    public class AppDbContext : IdentityDbContext<IdentityUser>
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)  //t passes those options to the base DbContext class using base(options).
         {
             //might include things like the connection string and DB provider (e.g., SQL Server, PostgreSQL, etc.).
         }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
       
 
