02505ebea33a1bdb55c73f032adcc9877b6fb41f
Samiul Haque Atul <samhaque999@gmail.com>
2025-04-25 02:47:18 +0600

    add controller and login,register UI 56,57

---
+++ Mango.Web/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Web.Controllers
+{
+    public class AuthController : Controller
+    { 
+        private readonly IAuthService _authService;
+
+        public AuthController( IAuthService authService) 
+        { 
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login(LoginRequestDto obj)
+        {
+            LoginRequestDto loginRequestDto = new();
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+    }
+}
