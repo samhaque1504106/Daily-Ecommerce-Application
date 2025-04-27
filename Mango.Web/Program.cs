12ebb06d9307c81b3da3b102215568da0b218334
Samiul Haque Atul <samhaque999@gmail.com>
2025-04-28 00:30:33 +0600

    61. adding token provider

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -1,6 +1,7 @@
 using Mango.Web.Service;
 using Mango.Web.Service.IService;
 using Mango.Web.Utility;
+using System.Security.Principal;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +22,7 @@ SD.AuthAPIBase = builder.Configuration["ServiceUrls:AuthAPI"];
 builder.Services.AddScoped<IBaseService, BaseService>();
 builder.Services.AddScoped<ICouponService, CouponService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<ITokenProvider, TokenProvider>();
 
 var app = builder.Build();
 
